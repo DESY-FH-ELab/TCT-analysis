282866b8c65ec439bafa0afb5d461c0f32fa3a40
TCThenna <hendrik.jansen@desy.de>
2014-07-06 19:23:57 +0200

    made main.cc compile
    - dummySample has << operator
    - added constructor to sample class

---
+++ src/sample.cc
@@ -0,0 +1,17 @@
+/**
+ * \file
+ * \brief Implementation of sample methods
+ */
+
+//#include "rambo.h"
+
+namespace TCT {
+
+	void ReadSampleCard(std::string folder){
+	
+	//read file from folder, fill thickness, effective doping etc
+		
+		std::cout << "reading SampleCard from " << folder << std::endl;
+		thickness = 500.0;
+	}
+}
