623e3190c23d42c5fc3811187022f8ff2653a36d
Hanno Perrey <hanno.perrey@desy.de>
2014-07-07 14:31:59 +0200

    fixed project name inconsistencies in CMake
    - now version name is correctly found in config.h

--- cmake/config.cmake.h
+++ cmake/config.cmake.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // CMake uses config.cmake.h to generate config.h within the build folder.
-#ifndef EUDAQ_CONFIG_H
-#define EUDAQ_CONFIG_H
+#ifndef TCTANALYSIS_CONFIG_H
+#define TCTANALYSIS_CONFIG_H
 
 #define PACKAGE_NAME "@CMAKE_PROJECT_NAME@"
-#define PACKAGE_VERSION "@EUDAQ_LIB_VERSION@"
+#define PACKAGE_VERSION "@TCTANALYSIS_LIB_VERSION@"
 #define PACKAGE_STRING PACKAGE_NAME " " PACKAGE_VERSION
 
 #define PACKAGE_SOURCE_DIR "@PROJECT_SOURCE_DIR@"
