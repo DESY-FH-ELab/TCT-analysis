bc37cedc5d89e82353c8161f0fc817f5f0f7400b
TCThenna <hendrik.jansen@desy.de>
2014-09-15 14:29:01 +0200

    add SetParameters method
    - read IDs and values from map

--- include/sample.h
+++ include/sample.h
@@ -6,72 +6,77 @@
 #ifndef __SAMPLE_H__
 #define __SAMPLE_H__ 1
 
-//  includes from standard library
+//  STD includes
 #include <iostream>
-//#include <vector>
-#include<string>
+#include <string>
+#include <map>
 
-//  includes from TCT classes
+//  TCT includes
 //#include "pulse.h"
 //#include "laser.h"
 
 
 namespace TCT {
 
-	// \brief Sample class for sample implementation	
-	
-	class sample {
+  // \brief Sample class for sample implementation	
 
-	private :
-		std::string _SampleID;	// identifier for sample
-		std::string _Folder;	// folder where data (property, not measurement of the sample is stored
-		//std::string _file;	// all measurement files belong to measurement class, not to sample class
-		double _Thickness;
+  class sample {
 
-
-	public :
-
-		// the empty constructor sets a dummy strong and a dummy thickness
-		sample() : 
-			_SampleID("DummyString"),
-			_Thickness(-1)
-		{}
+    private :
+      std::string _SampleID;	// identifier for sample
+      std::string _Folder;	// folder where sample card is stored
+      double _Thickness;
 
 
-		sample(std::string folder) : 
-			_Folder(folder),
-			_Thickness(-1)
-		{}
+    public :
 
-		// Default copy constructer should be fine
-		sample(const sample &)               = default;
-		sample & operator = (const sample &) = default;
-
-		// Dectructor
-		~sample() = default;
-
-		double & Thickness(){ return _Thickness;}
-		const double & Thickness() const{ return _Thickness;}
-
-		std::string & SampleID() {return _SampleID;}
-		const std::string & SampleID() const {return _SampleID;}
-
-		void SetThickness(double thick) { _Thickness = thick;}
-		void SetSampleID(std::string sampleID) { _SampleID = sampleID;}
+      // the empty constructor sets a dummy strong and a dummy thickness
+      sample() : 
+	_SampleID("DummyString"),
+	_Thickness(-1){}
 
 
-		void SetFolder(std::string folder) {_Folder = folder;}
-		std::string & Folder() { return _Folder;}
-		const std::string & Folder() const { return _Folder;}
+      sample(std::string folder) : 
+	_Folder(folder),
+	_Thickness(-1){}
 
-		void ReadSampleCard();
-	};
+      sample(std::map<std::string, std::string> id_val) : 
+	sample()
+      {
+        SetParameters(id_val);        
+      }
+
+      // Default copy constructer should be fine
+      sample(const sample &)               = default;
+      sample & operator = (const sample &) = default;
+
+      // Dectructor
+      ~sample() = default;
+
+      double Thickness(){ return _Thickness;}
+      const double & Thickness() const{ return _Thickness;}
+
+      std::string SampleID() {return _SampleID;}
+      const std::string & SampleID() const {return _SampleID;}
+
+      //void SetThickness(double thick) { _Thickness = thick;}
+      //void SetSampleID(std::string sampleID) { _SampleID = sampleID;}
+
+      std::string Folder() { return _Folder;}
+      void SetFolder(std::string folder) {_Folder = folder;}
+      const std::string & Folder() const { return _Folder;}
+
+      void SetParameters(std::map<std::string, std::string> id_val);
+
+  };
 
 }
 
 inline std::ostream & operator << (std::ostream & os, const TCT::sample & sam) {
-	return os 	<< "\n This sample is called " << sam.SampleID()
-			<< "\n   - SampleCardFolder = " << sam.Folder() 
-			<< "\n   - thickness = " << sam.Thickness() << std::endl;
+  return os 	<< "\n This sample is called " << sam.SampleID()
+    << "\n   - SampleCardFolder = " << sam.Folder() 
+    << "\n   - Sample ID = " << sam.SampleID() 
+    << "\n   - thickness = " << sam.Thickness() 
+    << std::endl;
 }
 #endif
--- src/sample.cc
+++ src/sample.cc
@@ -3,23 +3,32 @@
  * \brief Implementation of sample methods
  */
 
+//  STD includes
 #include<string>
 
+//  TCT includes
 #include "sample.h"
 
 namespace TCT {
 
-  void sample::ReadSampleCard(){
+  void sample::SetParameters(std::map<std::string, std::string> id_val){
 
-    //read file from folder. fill name, thickness, effective doping etc
+#ifdef DEBUG 
+    std::cout << " start SAMPLE::SetParameterd: Read and set parameters from input map" << std::endl; 
+#endif
 
-    std::cout << "\n reading SampleCard from " << _Folder << std::endl;
-    double thick = 320.; // rad this from card
-    SetThickness(thick);
+    for( auto i : id_val){
+      //if(i.first == "") _ = atof((i.second).c_str());
+      if(i.first == "SampleCardFolder") _Folder = i.second;
+      if(i.first == "SampleID") _SampleID = i.second;
+      if(i.first == "Thickness") _Thickness = atof((i.second).c_str());
+    }
 
-    std::string sampleID = "DummySample1"; // read this from card
-    SetSampleID(sampleID); 
+#ifdef DEBUG 
+    std::cout << " end SAMPLE::SetParameters" << std::endl; 
+#endif
+
+    return;
   }
-
 
 }
